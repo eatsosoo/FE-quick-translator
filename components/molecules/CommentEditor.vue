4bfb7d808a0f23a7d3417b37d5a4145177dab3ac
PhongTD <eatsoosoo@gmail.com>
2024-11-19 16:17:31 +0700

    feat: Refactor CommentEditor to support dynamic GIF loading and toolbar integration; enhance GIF selection functionality

--- components/molecules/CommentEditor.vue
+++ components/molecules/CommentEditor.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
     <div class="mt-2 relative">
-      <img
-        src="http://danda.vn/meme/rabbit/001.gif"
-        alt="gif_open"
-        class="w-8 h-8 cursor-pointer"
-        @click="toggleGifList"
-      />
+      <div v-for="toolbar in toolbars" :key="toolbar.name">
+        <img
+          :src="toolbar.present"
+          :alt="`gifs_${toolbar.name}`"
+          class="w-8 h-8 cursor-pointer"
+          @click="toggleGifList(toolbar.name)"
+        />
+      </div>
       <div
         v-if="showGifList"
         class="flex flex-wrap w-[20rem] min-h-10 border p-2 absolute top-[2.5rem] left-[1rem] z-5 bg-white rounded-md shadow-md"
       >
-        <Loader2 v-if="gifs.length === 0" class="w-4 h-4 mr-2 animate-spin" />
-        <template v-else>
-          <div v-for="gif in gifs" :key="gif.id" @click="selectGif(gif)">
-            <img :src="gif.url" alt="gif" class="w-8 h-8" />
+          <div v-for="(gifUrl, index) in currentGifs" :key="index">
+            <img :src="gifUrl" alt="gif" class="w-8 h-8" @click="selectGif(gifUrl)"/>
           </div>
-        </template>
       </div>
     </div>
     <div
@@ -33,38 +32,39 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { Loader2 } from "lucide-vue-next";
 
 const editor = ref<HTMLElement | null>(null);
 const showGifList = ref(false);
-const gifs = ref<{ id: string; url: string }[]>([]);
+const gifsAll = ref<{[key: string]: string[]}>({});
+const currentGifs = ref<string[]>([]);
 const content = ref("");
+const toolbars = ref<{ name: string, present: string}[]>([]);
 
-const fetchGifs = async () => {
-  if (gifs.value.length > 0) return;
-
+const fetchOrigins = async () => {
   const response = await fetch("http://demo.danda.vn/api/memes");
   const data = await response.json();
-  gifs.value = data.rabbit.map((gif: string, index: string) => {
-    return {
-      id: index,
-      url: `http://danda.vn/meme/rabbit/001.gif`,
-      // url: `http://danda.vn${gif}`,
-    };
-  });
-};
+  toolbars.value = data;
+}
+fetchOrigins();
 
-const toggleGifList = () => {
+const toggleGifList = async (key: string) => {
   showGifList.value = !showGifList.value;
-  if (showGifList.value) {
-    fetchGifs();
+
+  if (gifsAll.value[key]) {
+    currentGifs.value = gifsAll.value[key];
+    return;
   }
+  
+  const response = await fetch(`http://demo.danda.vn/api/memes/${key}`);
+  const data = await response.json();
+  gifsAll.value[key] = data;
+  currentGifs.value = gifsAll.value[key];
 };
 
-const selectGif = (gif: { id: string; url: string }) => {
+const selectGif = (gifUrl: string ) => {
   if (editor.value) {
     const img = document.createElement("img");
-    img.src = gif.url;
+    img.src = gifUrl;
     img.alt = "gif";
     img.className = "w-8 h-8";
     editor.value.appendChild(img);
